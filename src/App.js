8727f3d44a7382d65ef084db0194b0795d83601e
unknown <53538316+grlucy@users.noreply.github.com>
2020-03-27 11:29:25 -0400

    added react files and set up component file structure

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <div className="mainContentDiv">
+        <h1>Header, SearchForm, and SearchResults go here</h1>
+      </div>
+      <h4>Footer goes here</h4>
+    </>
+  );
+}
+
+export default App;
